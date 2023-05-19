b524edc04fc36728f59fdef775b7e7edefa8e018
Bidyut mazumder <46864109+BidyutMazumder@users.noreply.github.com>
2023-05-19 23:27:41 +0600

    Add project files.

---
+++ demoDotNetCoreAPI/Controllers/PostController1.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace demoDotNetCoreAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PostController1 : ControllerBase
+    {
+    }
+}
---
+++ demoDotNetCoreAPI/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace demoDotNetCoreAPI.Controllers
+{
+    [Route("api/controller")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [Route("[action]")]
+        [HttpGet]
+        public string GetName()
+        {
+            return "jamal";
+        }
+        [Route("kamal")]
+        [HttpPost]
+        public string PostName()
+        {
+            return "kamal";
+        }
+    }
+}
